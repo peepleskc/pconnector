83e720bfd0ae3f0368637db6d5c297ba2139f2d1
Kaycee <peepleskc@yahoo.com>
2019-03-26 20:27:26 -0400

    asdfasdf

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Server static assests if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -35,15 +44,6 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
-
-// Server static assests if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 8000;
 
